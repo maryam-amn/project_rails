23fd9bd262d4afcd8106543bcc7ce7edafca5a37
maryam-amn <meryem.2005.aman@gmail.com>
2026-04-02 17:23:47 +0200

    fix: correction of the schema file

--- db/migrate/20260402151754_create_characters.rb
+++ db/migrate/20260402151754_create_characters.rb
@@ -3,11 +3,13 @@ class CreateCharacters < ActiveRecord::Migration[8.1]
     create_enum :regions, %w[Liyue Fontaine Montstadt]
 
     create_table :characters do |t|
-      t.string :name, null: false
-      t.text :description, null: false
-      t.integer :rarity, null: false
-      t.enum :region, enum_type: "regions", null: false
+
+        t.string :name, null: false
+        t.text :description, null: false
+        t.integer :rarity, null: false
+        t.enum :region, enum_type: "regions", null: false
       t.timestamps
-    end
+
+      end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_26_162604) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_02_151754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  # Custom types defined in this database.
+  # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum "regions", ["Liyue", "Fontaine", "Montstadt"]
+
   create_table "characters", force: :cascade do |t|
-    t.decimal "average_rating"
     t.datetime "created_at", null: false
-    t.text "description"
-    t.string "name"
-    t.integer "rarity"
-    t.string "region"
-    t.string "type"
+    t.text "description", null: false
+    t.string "name", null: false
+    t.integer "rarity", null: false
+    t.enum "region", null: false, enum_type: "regions"
     t.datetime "updated_at", null: false
   end
 end
