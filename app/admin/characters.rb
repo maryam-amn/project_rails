0e436a5357bce812e6657a2285d52eea3d9e91a0
maryam-amn <meryem.2005.aman@gmail.com>
2026-04-22 15:42:31 +0200

    feat: add a button to be able to create a character Closes #8

--- app/admin/characters.rb
+++ app/admin/characters.rb
@@ -11,4 +11,26 @@ ActiveAdmin.register Character do
     column :description
     actions
   end
+  form title: I18n.t("Characters.new.add") do |f|
+    f.object.errors.full_messages
+    inputs "Add a new character" do
+      f.li    I18n.t("Characters.new.info"), style: "font-weight: 750;"
+      f.input :name
+      f.input :region, as: :select, required: true, collection: Character.regions.keys, style: "width: 50%;", label: "Region"
+      f.input :rarity, as: :number, required: true, placeholder: I18n.t("Characters.errors.number_between_0_1")
+      f.input :description, as: :text, required: true
+    end
+    actions
+  end
+  controller do
+    def create
+      @character = Character.new(permitted_params[:character])
+      if @character.save
+        flash[:notice] = I18n.t("Characters.new.created")
+        redirect_to admin_characters_url
+      else
+        render :new, status: :unprocessable_content
+      end
+    end
+  end
 end
--- app/controllers/admin/character_controller.rb
+++ app/controllers/admin/character_controller.rb
@@ -1,2 +1,6 @@
 class Admin::CharacterController < ApplicationController
+=begin
+  we can't remove this file,
+  it's useful to be able to run test character_controller_test (these test check that our create and view works as it should)
+=end
 end
