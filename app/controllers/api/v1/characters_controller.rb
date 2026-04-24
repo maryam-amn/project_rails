73802bbb89b414715bfe5b6fd25246cc5447756f
maryam-amn <meryem.2005.aman@gmail.com>
2026-04-24 16:55:17 +0200

    feat: Add an endpoint to the API to view all characters Closes #20

---
+++ app/controllers/api/v1/characters_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+class  Api::V1::CharactersController < ActionController::Base
+  def index
+    @characters = Character.all
+
+    render json:  { characters:
+       @characters.map do |character|
+         {
+           id: character.id.to_i,
+           name: character.name.to_s,
+           description: character.description.to_s,
+           rarity: character.rarity.to_i,
+           region: character.region.to_s
+         }
+       end
+    }
+  end
+end
